fdcf8014105de2eb1bdca5a9a66713b4e891b5fd
Sebastien-14 <contactmisterkhan@gmail.com>
2025-08-24 14:30:01 +0200

    Update main.py

--- main.py
+++ main.py
@@ -179,6 +179,24 @@ async def panel_cmd(ctx):
     await channel.send(embed=embed, view=view)
     await ctx.send("Panneau envoyé.")
 
+import threading
+from flask import Flask
+import os
+
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "Bot is alive!"
+
+def run():
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port)
+
+# Démarrer le serveur dans un thread séparé
+threading.Thread(target=run).start()
+
+
 # --- Lancer le bot ---
 bot.run(TOKEN)
 
